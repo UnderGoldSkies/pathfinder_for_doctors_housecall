8dbd2e54e0859ea6d335814e8c0bd7ac5723e42c
Jacklim <jackweixuan@gmail.com>
2024-02-16 15:52:16 +0800

    Version 0.1 completed, going deployment

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ import numpy as np
 import pandas as pd
 import itertools
 import datetime
+import streamlit.components.v1 as components
 
-import pydeck as pdk
 from datetime import datetime
 from itertools import permutations
 
-google_api_key =
+from htmlTemplates import directions_map
+
+google_api_key = st.secrets["google_api_key"]
+
 gmaps = googlemaps.Client(key=google_api_key)
 
+
 @st.cache_data
-def generate_permutation_duration(permutations_list,postal_code_distance_matrix_dict):
+def generate_permutation_duration(permutations_list,postal_code_distance_matrix_dict,postal_code_list):
     duration_of_full_route_dict = {}
     for possible_routes in permutations_list:
         duration_of_full_route=[]
-        if possible_routes[0] == '119228' and possible_routes[-1] == '119228':
+        if possible_routes[0] == postal_code_list[0] and possible_routes[-1] == postal_code_list[-1]:
             for index in range(len(possible_routes)-1):
                 ori_dest_pair = f"{possible_routes[index]},{possible_routes[index+1]}"
                 if ori_dest_pair in postal_code_distance_matrix_dict:
@@ -52,7 +56,7 @@ def generate_pair_distance(postal_code_list):
 def generate_textboxes(num_textboxes):
     textboxes = []
     for i in range(num_textboxes):
-        textbox_value = st.text_input(f"Postal Code {i + 1}", f"119228")
+        textbox_value = st.text_input(f"Postal Code {i + 1}", f"119074")
         textboxes.append(textbox_value)
     return textboxes
 
@@ -64,45 +68,18 @@ def validate_postal_code(postal_code):
 def main():
 
         # header
-    st.header("Dr Ant-Thony")
+    st.title("Dr Ant-Thony")
+    mode = "place"
+    location = "119074"
+    st.write("Empowering Healing Journeys: Your AI Pathfinder for Optimal Patient Care, Navigating the Roads of Compassion with Efficiency")
 
-    st.subheader("Optimizing your home visits route")
+    shortest_route = None
 
     # Validation flag
     all_validated = True
 
     postal_code_distance_matrix_dict ={}
     duration_of_full_route_dict ={}
-
-
-    location = [37.7749, -122.4194]
-    # Create a Google Maps Deckgl layer
-    layer = pdk.Layer(
-        "GoogleMapsLayer",
-        google_api_key,
-        zoom=12,
-        center=location,
-        id="google-maps",
-    )
-
-    # Create a Pydeck Deck instance
-    view_state = pdk.ViewState(
-        latitude=location[0],
-        longitude=location[1],
-        zoom=12,
-        bearing=0,
-        pitch=45,
-    )
-
-    r = pdk.Deck(
-        layers=[layer],
-        initial_view_state=view_state,
-    )
-
-    # Display the map using st
-    st.pydeck_chart(r)
-
-
 
     with st.sidebar:
         st.subheader("List of Postal Codes to Visit")
@@ -128,20 +105,20 @@ def main():
                     with st.spinner("Ant-Thony is Working on it"):
                         postal_code_distance_matrix_dict = generate_pair_distance(postal_code_list)
                         permutations_list = list(permutations(postal_code_list))
-                        duration_of_full_route_dict = generate_permutation_duration(permutations_list,postal_code_distance_matrix_dict)
+                        duration_of_full_route_dict = generate_permutation_duration(permutations_list,postal_code_distance_matrix_dict,postal_code_list)
                         shortest_route = min(duration_of_full_route_dict, key=duration_of_full_route_dict.get)
-                        #st.write(f"duration_of_full_route_dict : {duration_of_full_route_dict}")
-                        st.write(f'shortest_route : {shortest_route}')
+                        shortest_route = shortest_route.replace("(","")
+                        shortest_route = shortest_route.replace(")","")
+                        shortest_route = shortest_route.split(",")
+
+
+    if shortest_route != None:
+        for i in range(len(shortest_route)-1):
+            st.write(f"Order {i+1}")
+            st.components.v1.html(directions_map.format(ori=shortest_route[i],dest=shortest_route[i+1],google_api_key=google_api_key), height=400)
 
 
 
-
-
-        # # Check if the Analyse button is pressed
-        # if st.button("Optimize"):
-
-
-        #      with st.spinner("Analyzing"):
 
 
 if __name__ == '__main__':
---
+++ htmlTemplates.py
@@ -0,0 +1,10 @@
+directions_map = """
+<iframe
+  width="700"
+  height="500"
+  frameborder="0" style="border:0"
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/directions?key={google_api_key}&origin={ori}&destination={dest}&mode=driving&region=SG"
+  allowfullscreen>
+</iframe>
+"""
